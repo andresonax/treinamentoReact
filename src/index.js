a7dca901269b6762fdaab584180ffdd306393482
Andre Rabelo <sepeticefet@gmail.com>
2019-05-30 17:04:23 -0300

    Atividades Dia 30/05/2019

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB} from './componentes/DoisComponentes'
+import  MultiElemento from './componentes/MultiElementos'
+import FamiliaSilva from './componentes/FamiliaSilva'
+import Famila from './componentes/Familia'
+import Membro from './componentes/Membro'
+import ComponenteComFuncao from './componentes/ComponenteComFuncao'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const elemento = document.getElementById("root")
+
+//ReactDOM.render(<h1>Ola mundo novamente!</h1>, elemento)
+  
+ReactDOM.render(
+    <ComponenteComFuncao />
+    // <Famila sobrenome = 'Rabelo'>
+    //     <Membro nome = 'Andre' />
+    //     <Membro nome = 'Caio' />
+    // </Famila>
+    // <div>
+    //     <PrimeiroComponente valor="Bom dia!" treino = {2**3} />
+    //     <CompB />
+    //     <CompA />
+    // </div>
+    // <div>
+    //     <FamiliaSilva />
+    // </div>
+    
+    
+    ,elemento)
